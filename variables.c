4a0113d16493702d098ff63523140e28dd7323b2
yashaswi-ece <yashusai543@gmail.com>
2026-09-02 11:23:05 +0530

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 18;
+    float height = 5.5;
+    char grade = 'A';
+
+    printf("Age = %d\n", age);
+    printf("Height = %.1f\n", height);
+    printf("Grade = %c\n", grade);
+
+    return 0;
+}
