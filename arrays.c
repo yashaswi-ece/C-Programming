4ec2d384e18aed2bf3937af58c1dd62d6427c49a
yashaswi-ece <yashusai543@gmail.com>
2026-09-02 11:35:12 +0530

    Create arrays.c

---
+++ arrays.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[5] = {10, 20, 30, 40, 50};
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        printf("%d\n", numbers[i]);
+    }
+
+    return 0;
+}
