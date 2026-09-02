0b6ff41988210bd3d09831bc530c1845eaf03b96
yashaswi-ece <yashusai543@gmail.com>
2026-09-02 11:27:01 +0530

    Create operators.c

---
+++ operators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 3;
+
+    printf("Addition = %d\n", a + b);
+    printf("Subtraction = %d\n", a - b);
+    printf("Multiplication = %d\n", a * b);
+    printf("Division = %d\n", a / b);
+    printf("Remainder = %d\n", a % b);
+
+    return 0;
+}
